1f21c0cddc2f3afc4f9c62aa2df24d1b8ec96171
Oscar Kevin <mtfallsVR@atemosta.com>
2022-04-17 15:55:52 -0400

    Tillia/VRTK Imports into WebXR Sample Scene (#1)
    * Tillia Imports
    
    * Setting up WebXR with VRTK4 in Unity3D - Grip Action
    
    * WebXR with VRTK - Teleport
    
    * WebXR with VRTK - Axis Move

---
+++ Assets/XR/WebXRAddon/WebThumb.cs
@@ -0,0 +1,10 @@
+using Zinnia.Action;
+using WebXR;
+public class WebThumb : BooleanAction
+{
+    public WebXRController controller;
+    void Update()
+    {
+        Receive(controller.GetButton(WebXRController.ButtonTypes.Thumbstick));
+    }
+}
